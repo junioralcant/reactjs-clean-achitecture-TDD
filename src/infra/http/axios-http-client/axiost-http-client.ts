4afb47add47c1f1985aece6054a235f49b2566a2
junioralcant <junioralcant111@gmail.com>
2022-11-13 17:11:11 -0300

    feat: ensure AxiosHttpClient calls axios with body

--- src/infra/http/axios-http-client/axiost-http-client.ts
+++ src/infra/http/axios-http-client/axiost-http-client.ts
@@ -2,7 +2,7 @@ import { HttpPostParams } from '../../../data/protocols/http';
 import axios from 'axios';
 
 export class AxiosHttpClient {
-  async post(params: HttpPostParams<any>): Promise<void> {
-    await axios.post(params.url);
+  async post({ url, body }: HttpPostParams<any>): Promise<void> {
+    await axios.post(url, body);
   }
 }
