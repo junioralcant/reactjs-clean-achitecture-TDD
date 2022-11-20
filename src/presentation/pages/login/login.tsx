f0c3c46cf1e020ddcc4d6aaf95f51cbf737b47e0
junioralcant <junioralcant111@gmail.com>
2022-11-20 09:23:45 -0300

    feat: prevent Login to call submit more than once

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -35,6 +35,11 @@ export function Login({ validation, authentication }: Props) {
     event: React.FormEvent<HTMLFormElement>
   ): Promise<void> {
     event.preventDefault();
+
+    if (state.isLoding) {
+      return;
+    }
+
     setState({
       ...state,
       isLoding: true,
