ca58252f885dbbaee47d9bca2ead01cd98838224
junioralcant <junioralcant111@gmail.com>
2022-11-22 17:35:15 -0300

    test: ensure RequiredFieldValidation returns falsy if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,3 +1,5 @@
+import {faker} from '@faker-js/faker';
+
 import {RequiredFieldError} from '../errors';
 import {RequeriedFileValidation} from './required-field-validation';
 
@@ -6,5 +8,11 @@ describe('RequiredFieldValidation', () => {
     const sut = new RequeriedFileValidation('email');
     const error = sut.validate('');
     expect(error).toEqual(new RequiredFieldError());
+  });
+
+  it('Shold return falsy if field is not empty', () => {
+    const sut = new RequeriedFileValidation('email');
+    const error = sut.validate(faker.internet.email());
+    expect(error).toBeFalsy();
   });
 });
