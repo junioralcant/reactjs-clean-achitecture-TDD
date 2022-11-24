67cb66bce55f3fb9bafdf87460a56076f8974a68
junioralcant <junioralcant111@gmail.com>
2022-11-23 22:11:49 -0300

    test: ensure MinLenghtValidation return error if value is invalid

---
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -0,0 +1,10 @@
+import {InvalidFieldError} from '../../errors/invalid-field-error';
+import {MinLengthValidation} from './min-length-validation';
+
+describe('MinLengthValidation', () => {
+  it('Should return error if value is invalid', () => {
+    const sut = new MinLengthValidation('field', 5);
+    const error = sut.validate('123');
+    expect(error).toEqual(new InvalidFieldError());
+  });
+});
