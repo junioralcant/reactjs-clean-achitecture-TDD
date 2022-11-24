8c0a02bcac5bd3b796532995643a1be7c78ea5c1
junioralcant <junioralcant111@gmail.com>
2022-11-23 21:55:40 -0300

    test: ensure EmailValidation return falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,17 @@
+import {faker} from '@faker-js/faker';
 import {InvalidFieldError} from '../../errors';
 import {EmailValidation} from './email-validation';
 
 describe('EmailValidation', () => {
   it('Should return error if email is invalid', () => {
-    const sut = new EmailValidation('email');
-    const error = sut.validate('');
+    const sut = new EmailValidation(faker.random.word());
+    const error = sut.validate(faker.random.word());
     expect(error).toEqual(new InvalidFieldError());
+  });
+
+  it('Should return falsy if email is valid', () => {
+    const sut = new EmailValidation(faker.internet.email());
+    const error = sut.validate(faker.internet.email());
+    expect(error).toBeFalsy();
   });
 });
