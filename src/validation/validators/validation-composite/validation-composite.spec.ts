836395e5a5b9a8f69b70beafecd385270c994be1
junioralcant <junioralcant111@gmail.com>
2022-11-25 18:26:15 -0300

    refactor: move SUT creation to a factory method

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,18 +1,30 @@
 import {FieldValidationSpy} from '../test/mack-field-validation';
 import {ValidationComposite} from './validation-composite';
 
+type SutTypes = {
+  sut: ValidationComposite;
+  fieldValidationSpy: FieldValidationSpy[];
+};
+
+function makeSut(): SutTypes {
+  const fieldValidationSpy = [
+    new FieldValidationSpy('any_field'),
+    new FieldValidationSpy('any_field'),
+  ];
+
+  const sut = new ValidationComposite(fieldValidationSpy);
+  return {
+    sut,
+    fieldValidationSpy,
+  };
+}
+
 describe('ValidationComposite', () => {
   it('Should return error if any validation falsy', () => {
-    const fieldValidationSpy = new FieldValidationSpy('any_field');
-    const fieldValidationSpy2 = new FieldValidationSpy('any_field');
+    const {sut, fieldValidationSpy} = makeSut();
 
-    fieldValidationSpy.error = new Error('first_error_message');
-    fieldValidationSpy2.error = new Error('second_error_message');
-
-    const sut = new ValidationComposite([
-      fieldValidationSpy,
-      fieldValidationSpy2,
-    ]);
+    fieldValidationSpy[0].error = new Error('first_error_message');
+    fieldValidationSpy[1].error = new Error('second_error_message');
 
     const error = sut.validate('any_field', 'any_value');
     expect(error).toBe('first_error_message');
