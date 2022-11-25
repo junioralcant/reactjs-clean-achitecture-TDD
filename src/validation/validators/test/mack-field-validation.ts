70c02350c466fdcc31ec032db841c3846e087248
junioralcant <junioralcant111@gmail.com>
2022-11-25 18:12:22 -0300

    feat: ensure ValidationComposite returns error if any validation fails

---
+++ src/validation/validators/test/mack-field-validation.ts
@@ -0,0 +1,10 @@
+import {IFielValidation} from '../../protocols/field-validation';
+
+export class FieldValidationSpy implements IFielValidation {
+  error: Error | null = null;
+
+  constructor(readonly field: string) {}
+  validate(value: string): Error | null {
+    return this.error;
+  }
+}
---
+++ src/validation/validators/validation-composite/validation-composite.ts
@@ -0,0 +1,29 @@
+import {IValidation} from '../../../presentation/protocols/validation';
+import {IFielValidation} from '../../protocols/field-validation';
+
+export class ValidationComposite implements IValidation {
+  constructor(private readonly validators: IFielValidation[]) {}
+
+  validate(fieldName: string, fieldValue: string): string {
+    const validators = this.validators.filter(
+      (validation) => validation.field === fieldName
+    );
+
+    for (const validator of validators) {
+      const error = validator.validate(fieldValue);
+      if (error) {
+        return error.message as string;
+      }
+    }
+
+    return '';
+  }
+
+  getMessageError(): string {
+    throw new Error('Method not implemented.');
+  }
+
+  setMessageError(message: string): string {
+    throw new Error('Method not implemented.');
+  }
+}
