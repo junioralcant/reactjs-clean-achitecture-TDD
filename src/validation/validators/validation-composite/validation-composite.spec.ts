c708bdf5ee3c2b276fb369112f58575bc64eb4c4
junioralcant <junioralcant111@gmail.com>
2022-11-25 18:15:01 -0300

    test: ensure ValidationComposite returns the first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -6,13 +6,15 @@ describe('ValidationComposite', () => {
     const fieldValidationSpy = new FieldValidationSpy('any_field');
     const fieldValidationSpy2 = new FieldValidationSpy('any_field');
 
-    fieldValidationSpy2.error = new Error('any_error_message');
+    fieldValidationSpy.error = new Error('first_error_message');
+    fieldValidationSpy2.error = new Error('second_error_message');
+
     const sut = new ValidationComposite([
       fieldValidationSpy,
       fieldValidationSpy2,
     ]);
 
     const error = sut.validate('any_field', 'any_value');
-    expect(error).toBe('any_error_message');
+    expect(error).toBe('first_error_message');
   });
 });
