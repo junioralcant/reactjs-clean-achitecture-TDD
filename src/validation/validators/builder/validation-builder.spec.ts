728d82f446b29466bb2d793ed306c2d6d0b2afa6
junioralcant <junioralcant111@gmail.com>
2022-11-27 09:20:17 -0300

    test: ensure ValidationBuilder has a email helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,3 +1,4 @@
+import {EmailValidation} from '../email/email-validation';
 import {RequiredFieldValidation} from '../required-field/required-field-validation';
 import {ValidationBuilder} from './validation-builder';
 
@@ -10,5 +11,13 @@ describe('ValidationBuilder', () => {
     expect(validations).toEqual([
       new RequiredFieldValidation('any_field'),
     ]);
+  });
+
+  it('Should return RequiredFieldValidations', () => {
+    const validations = ValidationBuilder.field('any_field')
+      .email()
+      .build();
+
+    expect(validations).toEqual([new EmailValidation('any_field')]);
   });
 });
