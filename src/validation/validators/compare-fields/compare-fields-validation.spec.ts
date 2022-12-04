4cc0e035afd4cd875d7006b59bd9380b7b30c9d0
junioralcant <junioralcant111@gmail.com>
2022-12-04 14:53:47 -0300

    test: ensure CompareFieldsValidation returns falsy if validation succeds

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -12,6 +12,13 @@ function makeSut(valueToCompare: string): CompareFieldValidation {
 
 describe('CompareFieldValidation', () => {
   it('Shold return error if compare is invalid', () => {
+    const valueToCompare = faker.random.word();
+    const sut = makeSut(valueToCompare);
+    const error = sut.validate(valueToCompare);
+    expect(error).toBeFalsy();
+  });
+
+  it('Shold return falsy if comape id valid', () => {
     const sut = makeSut(faker.random.word());
     const error = sut.validate(faker.random.word());
     expect(error).toEqual(new InvalidFieldError());
