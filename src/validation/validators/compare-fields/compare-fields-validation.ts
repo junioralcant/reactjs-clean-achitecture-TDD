6efe85da3a81cb66c4d761e74b8d4c035a54d53a
junioralcant <junioralcant111@gmail.com>
2022-12-04 14:47:21 -0300

    feat: ensure CompareFieldsValidation returns error if validation fails

---
+++ src/validation/validators/compare-fields/compare-fields-validation.ts
@@ -0,0 +1,13 @@
+import {InvalidFieldError} from '../../errors';
+import {IFielValidation} from '../../protocols/field-validation';
+
+export class CompareFieldValidation implements IFielValidation {
+  constructor(
+    readonly field: string,
+    private readonly valueToCompare: string
+  ) {}
+
+  validate(value: string): Error | null {
+    return new InvalidFieldError();
+  }
+}
