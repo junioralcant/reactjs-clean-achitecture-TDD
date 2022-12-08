41c4215f9826215226352c1d015a8aa7bb997410
junioralcant <junioralcant111@gmail.com>
2022-12-08 19:38:44 -0300

    feat: add SignUpvalidationFactory

---
+++ src/presentation/factories/pages/signup/signup-validation-factory.ts
@@ -0,0 +1,14 @@
+import {ValidationComposite} from '../../../../validation/validators/validation-composite/validation-composite';
+import {ValidationBuilder as Builder} from '../../../../validation/validators/builder/validation-builder';
+
+export function makeSignUpValidation(): ValidationComposite {
+  return ValidationComposite.build([
+    ...Builder.field('name').required().min(3).build(),
+    ...Builder.field('email').required().email().build(),
+    ...Builder.field('password').required().min(5).build(),
+    ...Builder.field('passwordConfirmation')
+      .required()
+      .min(5)
+      .build(),
+  ]);
+}
