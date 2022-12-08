5f7b25287d4eaace8ea8a5267c9993074dab1df2
junioralcant <junioralcant111@gmail.com>
2022-12-08 19:53:22 -0300

    feat: add SignUp Composition

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import {makeLogin} from './presentation/factories/pages/login/login-factory';
+import {makeSignUp} from './presentation/factories/pages/signup/login-factory';
 import {Router} from './presentation/router/router';
 import './presentation/styles/global.scss';
 
 function App() {
-  return <Router MakeLogin={makeLogin} />;
+  return <Router MakeLogin={makeLogin} MakeSignUp={makeSignUp} />;
 }
 
 export default App;
--- src/presentation/router/router.tsx
+++ src/presentation/router/router.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import {SignUp} from '../pages/signup/signup';
 
-type Props = {
+type Factory = {
   MakeLogin: React.FC;
+  MakeSignUp: React.FC;
 };
 
-export function Router({MakeLogin}: Props) {
+export function Router({MakeLogin, MakeSignUp}: Factory) {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<MakeLogin />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signup" element={<MakeSignUp />} />
       </Routes>
     </BrowserRouter>
   );
