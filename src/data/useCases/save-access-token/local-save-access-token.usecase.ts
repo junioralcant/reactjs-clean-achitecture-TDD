78795b3f5ecf38dc7e677a35b2cd92d39de90515
junioralcant <junioralcant111@gmail.com>
2022-12-09 18:18:29 -0300

    feat: ensure LocalSaveAccessTokenUseCase throws if no accessToken is provided

--- src/data/useCases/save-access-token/local-save-access-token.usecase.ts
+++ src/data/useCases/save-access-token/local-save-access-token.usecase.ts
@@ -1,3 +1,4 @@
+import {UnexpectedError} from '../../../domain/errors';
 import {ISaveAccessToken} from '../../../domain/useCases/save-access-token';
 import {ISetStorage} from '../../protocols/cache/set-storage';
 
@@ -5,6 +6,9 @@ export class LocalSaveAccessTokenUseCase implements ISaveAccessToken {
   constructor(private readonly setStorage: ISetStorage) {}
 
   async save(accessToken: string): Promise<void> {
+    if (!accessToken) {
+      throw new UnexpectedError();
+    }
     this.setStorage.set('accessToken', accessToken);
   }
 }
