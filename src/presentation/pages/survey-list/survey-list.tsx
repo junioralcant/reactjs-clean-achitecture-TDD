57b2318f5d2163041c7281fecec2e064e9e417a9
junioralcant <junioralcant111@gmail.com>
2022-12-14 18:23:26 -0300

    feat: add skeleton shimmer effect

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -26,51 +26,9 @@ export function SurveyList() {
             </div>
             <footer>Ver resultado</footer>
           </li>
-          <li>
-            <div className="surveyContent">
-              <Icon
-                className="iconWrap"
-                iconName={IconName.thumbUp}
-              />
-              <time>
-                <span className="day">22</span>
-                <span className="month">03</span>
-                <span className="year">2022</span>
-              </time>
-              <p>Qual é o seu framework web favorito?</p>
-            </div>
-            <footer>Ver resultado</footer>
-          </li>
-          <li>
-            <div className="surveyContent">
-              <Icon
-                className="iconWrap"
-                iconName={IconName.thumbUp}
-              />
-              <time>
-                <span className="day">22</span>
-                <span className="month">03</span>
-                <span className="year">2022</span>
-              </time>
-              <p>Qual é o seu framework web favorito?</p>
-            </div>
-            <footer>Ver resultado</footer>
-          </li>
-          <li>
-            <div className="surveyContent">
-              <Icon
-                className="iconWrap"
-                iconName={IconName.thumbUp}
-              />
-              <time>
-                <span className="day">22</span>
-                <span className="month">03</span>
-                <span className="year">2022</span>
-              </time>
-              <p>Qual é o seu framework web favorito?</p>
-            </div>
-            <footer>Ver resultado</footer>
-          </li>
+          <li></li>
+          <li></li>
+          <li></li>
         </ul>
       </div>
 
