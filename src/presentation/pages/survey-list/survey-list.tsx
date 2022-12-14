1abadc5f3eaf955dffd48168e2d8b03fae620ee1
junioralcant <junioralcant111@gmail.com>
2022-12-14 15:24:31 -0300

    feat: add survey icon show status

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -20,50 +20,13 @@ export function SurveyList() {
         <ul>
           <li>
             <div className="surveyContent">
-              <time>
-                <span className="day">22</span>
-                <span className="month">03</span>
-                <span className="year">2022</span>
-              </time>
-              <p>Qual é o seu framework web favorito?</p>
-            </div>
-            <footer>Ver resultado</footer>
-          </li>
-          <li>
-            <div className="surveyContent">
-              <time>
-                <span className="day">22</span>
-                <span className="month">03</span>
-                <span className="year">2022</span>
-              </time>
-              <p>Qual é o seu framework web favorito?</p>
-            </div>
-            <footer>Ver resultado</footer>
-          </li>
-          <li>
-            <div className="surveyContent">
-              <time>
-                <span className="day">22</span>
-                <span className="month">03</span>
-                <span className="year">2022</span>
-              </time>
-              <p>Qual é o seu framework web favorito?</p>
-            </div>
-            <footer>Ver resultado</footer>
-          </li>
-          <li>
-            <div className="surveyContent">
-              <time>
-                <span className="day">22</span>
-                <span className="month">03</span>
-                <span className="year">2022</span>
-              </time>
-              <p>Qual é o seu framework web favorito?</p>
-            </div>
-            <footer>Ver resultado</footer>
-          </li>
-          <li>
-            <div className="surveyContent">
+              <div className="iconWrap green">
+                <img
+                  className="icon"
+                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA70lEQVQ4Ea2RPQoCQQyFZ/w5g72lYOEVPIiV2IkIHmCvIZ5D77BgZWtrYWe1ICiuL8tEwjIZZmYNZCf7knyTzRrjrK7rAfwAr+AheyNZwiei98gNrBkISxYjz5KbZb0V4gXxlN8jzo+1tk91BOT6nhPmOFNg1Nb0UiCNxY0Uu8QW044BuMIZHs3DJzcra3/yOgem3UoT3pEcaQUh3TchAX9/KNTsy/mAtLebrzhXI+AqE/oQl55ErIfYxp5WothW71QyAJ0VWKG06DJAQ/jTA0yH0TUAzf4Gc8BFC5g3GcHI3IQvBy0asesDsB08CfYFB/44kX6+Hj8AAAAASUVORK5CYII="
+                  alt=""
+                />
+              </div>
               <time>
                 <span className="day">22</span>
                 <span className="month">03</span>
