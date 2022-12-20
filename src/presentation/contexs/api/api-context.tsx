96fb64b993789eb51bc9b517d9d918db9bfee7c1
junioralcant <junioralcant111@gmail.com>
2022-12-20 17:20:52 -0300

    feat: add getCurrentAccount to ApiContext

--- src/presentation/contexs/api/api-context.tsx
+++ src/presentation/contexs/api/api-context.tsx
@@ -3,6 +3,7 @@ import {AccountModel} from '../../../domain/models';
 
 type Props = {
   setCurrentAccount(account: AccountModel | undefined): void;
+  getCurrentAccount?(): AccountModel;
 };
 
 export const ApiContext = createContext<Props>(null as any);
