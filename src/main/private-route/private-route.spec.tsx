047382db5b50673f1aac04d7413ef493be8a66c2
junioralcant <junioralcant111@gmail.com>
2022-12-22 19:05:24 -0300

    refactor: remove BrowserRouter

--- src/main/private-route/private-route.spec.tsx
+++ src/main/private-route/private-route.spec.tsx
@@ -1,7 +1,6 @@
 import {render} from '@testing-library/react';
 import {createMemoryHistory} from 'history';
 import {
-  BrowserRouter,
   Route,
   Routes,
   unstable_HistoryRouter as HistoryRouter,
