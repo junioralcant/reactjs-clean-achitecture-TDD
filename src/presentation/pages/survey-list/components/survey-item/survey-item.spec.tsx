c7788f72d538b2a20b4db9da994ecd235d84be66
junioralcant <junioralcant111@gmail.com>
2022-12-30 15:28:46 -0300

    refactor: move SUT creation to a facktory method

--- src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -3,12 +3,17 @@ import {mockSurveyModel} from '../../../../../domain/test';
 import {IconName} from '../../../../components/icon/icon';
 import {SurveyItem} from './survey-item';
 
+function makeSut(survey = mockSurveyModel()) {
+  render(<SurveyItem survey={survey} />);
+}
+
 describe('SurveyItem Component', () => {
   it('Should render with correct values', () => {});
-  const survey = mockSurveyModel();
-  survey.didAnswer = true;
-  survey.date = new Date('2022-12-30T00:00:00');
-  render(<SurveyItem survey={survey} />);
+  const survey = Object.assign(mockSurveyModel(), {
+    didAnswer: true,
+    date: new Date('2022-12-30T00:00:00'),
+  });
+  makeSut(survey);
 
   expect(screen.getByTestId('icon')).toHaveProperty(
     'src',
@@ -17,7 +22,6 @@ describe('SurveyItem Component', () => {
   expect(screen.getByTestId('question')).toHaveTextContent(
     survey.question
   );
-
   expect(screen.getByTestId('day')).toHaveTextContent('30');
   expect(screen.getByTestId('month')).toHaveTextContent('dez');
   expect(screen.getByTestId('year')).toHaveTextContent('2022');
