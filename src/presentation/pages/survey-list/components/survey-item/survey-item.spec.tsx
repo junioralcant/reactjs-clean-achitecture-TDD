230a74ca1f7d304121a21a25bed9315fdd37bea2
junioralcant <junioralcant111@gmail.com>
2022-12-30 15:36:49 -0300

    test: ensure SurveyItem load icon and day correctly

--- src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -8,21 +8,41 @@ function makeSut(survey = mockSurveyModel()) {
 }
 
 describe('SurveyItem Component', () => {
-  it('Should render with correct values', () => {});
-  const survey = Object.assign(mockSurveyModel(), {
-    didAnswer: true,
-    date: new Date('2022-12-30T00:00:00'),
-  });
-  makeSut(survey);
+  it('Should render with correct values', () => {
+    const survey = Object.assign(mockSurveyModel(), {
+      didAnswer: true,
+      date: new Date('2022-12-30T00:00:00'),
+    });
+    makeSut(survey);
 
-  expect(screen.getByTestId('icon')).toHaveProperty(
-    'src',
-    IconName.thumbUp
-  );
-  expect(screen.getByTestId('question')).toHaveTextContent(
-    survey.question
-  );
-  expect(screen.getByTestId('day')).toHaveTextContent('30');
-  expect(screen.getByTestId('month')).toHaveTextContent('dez');
-  expect(screen.getByTestId('year')).toHaveTextContent('2022');
+    expect(screen.getByTestId('icon')).toHaveProperty(
+      'src',
+      IconName.thumbUp
+    );
+    expect(screen.getByTestId('question')).toHaveTextContent(
+      survey.question
+    );
+    expect(screen.getByTestId('day')).toHaveTextContent('30');
+    expect(screen.getByTestId('month')).toHaveTextContent('dez');
+    expect(screen.getByTestId('year')).toHaveTextContent('2022');
+  });
+
+  it('Should render with correct values', () => {
+    const survey = Object.assign(mockSurveyModel(), {
+      didAnswer: false,
+      date: new Date('2021-05-12T00:00:00'),
+    });
+    makeSut(survey);
+
+    expect(screen.getByTestId('icon')).toHaveProperty(
+      'src',
+      IconName.thumbDown
+    );
+    expect(screen.getByTestId('question')).toHaveTextContent(
+      survey.question
+    );
+    expect(screen.getByTestId('day')).toHaveTextContent('12');
+    expect(screen.getByTestId('month')).toHaveTextContent('mai');
+    expect(screen.getByTestId('year')).toHaveTextContent('2021');
+  });
 });
