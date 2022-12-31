c3de7dcfac355b000055bb7b52da8b16a7160164
junioralcant <junioralcant111@gmail.com>
2022-12-31 10:15:48 -0300

    test: ensure SurveyList render SurveyItems on success

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,13 +1,15 @@
-import {render, screen} from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
 import {SurveyModel} from '../../../domain/models';
+import {mockSurveyListModel} from '../../../domain/test';
 import {ILoadSurveyList} from '../../../domain/useCases';
 import {SurveyList} from './survey-list';
 
 class LoadSurveyListSpy implements ILoadSurveyList {
   callsCount = 0;
+  surveys = mockSurveyListModel();
   async loadAll(): Promise<SurveyModel[] | undefined> {
     this.callsCount++;
-    return [];
+    return this.surveys;
   }
 }
 
@@ -24,14 +26,26 @@ function makeSut(): SutTypes {
 }
 
 describe('SurveyzList Component', () => {
-  it('Should present 4 empty items on start', () => {
+  it('Should present 4 empty items on start', async () => {
     makeSut();
     const surveyList = screen.getByTestId('survey-list');
     expect(surveyList.querySelectorAll('li:empty').length).toBe(4);
+    await waitFor(() => surveyList);
   });
 
-  it('Should call LoadSurveyList', () => {
+  it('Should call LoadSurveyList', async () => {
     const {loadSurveyListSpy} = makeSut();
     expect(loadSurveyListSpy.callsCount).toBe(1);
+    await waitFor(() => screen.getByText('Enquetes'));
+  });
+
+  it('Should render SurveyItems on sucess', async () => {
+    makeSut();
+    const surveyList = screen.getByTestId('survey-list');
+    await waitFor(() =>
+      expect(
+        surveyList.querySelectorAll('li.surveyItemWrap').length
+      ).toBe(3)
+    );
   });
 });
