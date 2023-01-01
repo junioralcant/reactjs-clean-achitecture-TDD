a2534e66480aa66003c5bf3f1ccc69c227da7601
junioralcant <junioralcant111@gmail.com>
2023-01-01 12:29:15 -0300

    feat: ensure AuthorizeHttpGetClientDecorator calls GetStorage with correct values

---
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -0,0 +1,21 @@
+import {IGetStorage} from '../../../data/protocols/cache/get-storage';
+import {mockHttpResponse} from '../../../infra/http/test/mock-axios';
+import {
+  HttpGetParams,
+  HttpReponse,
+  HttpStatusCode,
+  IHttpGetClient,
+} from '../../../data/protocols/http';
+
+export class AuthorizeHttpGetClientDecorator
+  implements IHttpGetClient
+{
+  constructor(private readonly getStorage: IGetStorage) {}
+  async get(params: HttpGetParams): Promise<HttpReponse> {
+    this.getStorage.get('account');
+    return {
+      statusCode: HttpStatusCode.ok,
+      body: undefined,
+    };
+  }
+}
