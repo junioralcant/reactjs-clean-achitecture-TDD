20f337a0ba14487f78bc36eb3babad9a128dd726
junioralcant <junioralcant111@gmail.com>
2023-01-12 18:11:58 -0300

    feat: add AuthorizeHttpGetClientDecorator factory

--- src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
@@ -1,11 +1,11 @@
 import {RemoteLoadSurveyList} from '../../../../data/useCases/load-survey-list/remote-load-survey-list.usecase';
 import {ILoadSurveyList} from '../../../../domain/useCases';
+import {makeAuthorizeHttpGetClientDecoratorFactory} from '../../decorators/authorize-http-get-client-decorator-factory';
 import {makeApiUrl} from '../../http/api-url-factory';
-import {makeAxiosHttpClient} from '../../http/axios-http-client-factory';
 
 export function makeRemoteLoadSurveyList(): ILoadSurveyList {
   return new RemoteLoadSurveyList(
     makeApiUrl('/surveys'),
-    makeAxiosHttpClient()
+    makeAuthorizeHttpGetClientDecoratorFactory()
   );
 }
