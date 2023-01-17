f6f9e050a1b703fe44e8b3179eb89faafa1c48a0
junioralcant <junioralcant111@gmail.com>
2023-01-17 10:49:32 -0300

    feat: ensure logout calls setCurrentAccount

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,14 +1,33 @@
+import React, {
+  AnchorHTMLAttributes,
+  DOMAttributes,
+  useContext,
+} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {ApiContext} from '../../contexs/api/api-context';
 import {Logo} from '../logo/logo';
 import './header-styles.scss';
 
 export function Header() {
+  const {setCurrentAccount} = useContext(ApiContext);
+  const navigate = useNavigate();
+
+  function logout(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) {
+    event.preventDefault();
+    setCurrentAccount(undefined);
+    navigate('/login');
+  }
   return (
     <header className="headerWrap">
       <div className="headerContent">
         <Logo />
         <div className="logoutWrap">
           <span>Rodrigo</span>
-          <a href="#">Sair</a>
+          <a data-testid="logout" href="#" onClick={logout}>
+            Sair
+          </a>
         </div>
       </div>
     </header>
