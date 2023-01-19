d93213db3ce2456ef6ce9d987aa6906cd808b520
junioralcant <junioralcant111@gmail.com>
2023-01-19 14:55:20 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/components/header/header.spec.tsx
+++ src/presentation/components/header/header.spec.tsx
@@ -1,4 +1,5 @@
 import {fireEvent, render, screen} from '@testing-library/react';
+import {AccountModel} from '../../../domain/models';
 import {mockAccountModel} from '../../../domain/test';
 import {ApiContext} from '../../contexs/api/api-context';
 import {Header} from './header';
@@ -9,21 +10,31 @@ jest.mock('react-router-dom', () => ({
   useNavigate: () => mockedUsedNavigate,
 }));
 
+type SutTypes = {
+  setCurrentAccountMock: (account: AccountModel) => void;
+};
+
+function makeSut(): SutTypes {
+  const setCurrentAccountMock = jest.fn();
+
+  render(
+    <ApiContext.Provider
+      value={{
+        setCurrentAccount: setCurrentAccountMock,
+        getCurrentAccount: mockAccountModel,
+      }}
+    >
+      <Header />
+    </ApiContext.Provider>
+  );
+  return {
+    setCurrentAccountMock,
+  };
+}
+
 describe('Header Component', () => {
   it('Should call setCurrentAccount with null value', () => {
-    const setCurrentAccountMock = jest.fn();
-
-    render(
-      <ApiContext.Provider
-        value={{
-          setCurrentAccount: setCurrentAccountMock,
-          getCurrentAccount: mockAccountModel,
-        }}
-      >
-        <Header />
-      </ApiContext.Provider>
-    );
-
+    const {setCurrentAccountMock} = makeSut();
     fireEvent.click(screen.getByTestId('logout'));
     expect(setCurrentAccountMock).toHaveBeenCalledWith(undefined);
     expect(mockedUsedNavigate).toHaveBeenCalledWith('/login');
