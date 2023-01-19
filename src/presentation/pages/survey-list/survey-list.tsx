a6848602536f8fa546300aa12032dace11187952
junioralcant <junioralcant111@gmail.com>
2023-01-19 15:44:34 -0300

    feat: ensute SurveyList logout on AccessDeniedError

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,17 +1,23 @@
 import {ILoadSurveyList} from '../../../domain/useCases';
 import {Footer} from '../../components/footer/footer';
 import {Header} from '../../components/header/header';
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useContext} from 'react';
 import {CreateContextSurvey} from './components/contex/contex';
 import {ListItem} from './components/list/list';
 import './survey-list-styles.scss';
 import {ErrorList} from './components/erro/error';
+import {AccessDeniedError} from '../../../domain/errors/access-denied-error';
+import {ApiContext} from '../../contexs/api/api-context';
+import {useNavigate} from 'react-router-dom';
 
 type Props = {
   loadSurveyList: ILoadSurveyList;
 };
 
 export function SurveyList({loadSurveyList}: Props) {
+  const navigate = useNavigate();
+  const {setCurrentAccount} = useContext(ApiContext);
+
   const [state, setState] = useState({
     surveys: [] as ILoadSurveyList.Model[] | undefined,
     error: '',
@@ -25,7 +31,12 @@ export function SurveyList({loadSurveyList}: Props) {
 
         setState({...state, surveys});
       } catch (error: any) {
-        setState({...state, error: error.message});
+        if (error instanceof AccessDeniedError) {
+          setCurrentAccount(undefined);
+          navigate('/login');
+        } else {
+          setState({...state, error: error.message});
+        }
       }
     }
 
