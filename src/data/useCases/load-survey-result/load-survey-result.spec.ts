a09061591644b6e6ea7d1553d69e4f7d82c0a19b
junioralcant <junioralcant111@gmail.com>
2023-04-05 15:51:58 -0300

    test: ensure LoadSurveyResult throws AccessDeniedErro on 403

--- src/data/useCases/load-survey-result/load-survey-result.spec.ts
+++ src/data/useCases/load-survey-result/load-survey-result.spec.ts
@@ -1,6 +1,8 @@
 import {faker} from '@faker-js/faker';
 import {RemoteLoadSurveyResult} from './load-survey-result';
 import {HttpGetClientSpy} from '../../test';
+import {HttpStatusCode} from '../../protocols/http';
+import {AccessDeniedError} from '../../../domain/errors/access-denied-error';
 
 type SutTypes = {
   sut: RemoteLoadSurveyResult;
@@ -23,5 +25,17 @@ describe('RemoteLoadSurveyResult', () => {
     const {sut, httpGetClientSpy} = makeSut(url);
     await sut.load();
     expect(httpGetClientSpy.url).toBe(url);
+  });
+
+  it('Should throw AccessDeniedError if HttpGetClient return 403', async () => {
+    const url = faker.internet.url();
+    const {sut, httpGetClientSpy} = makeSut(url);
+
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden,
+    };
+
+    const promise = sut.load();
+    await expect(promise).rejects.toThrow(new AccessDeniedError());
   });
 });
