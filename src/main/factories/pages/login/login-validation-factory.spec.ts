3e431f06c11a9d21d6c4422c4b1490dcff531b1f
junioralcant <junioralcant111@gmail.com>
2023-04-05 14:11:43 -0300

    refactor: change validation factory to not use builder

--- src/main/factories/pages/login/login-validation-factory.spec.ts
+++ src/main/factories/pages/login/login-validation-factory.spec.ts
@@ -1,20 +1,18 @@
 import {makeLoginValidation} from './login-validation-factory';
 import {ValidationComposite} from '../../../../validation/validators/validation-composite/validation-composite';
-import {ValidationBuilder} from '../../../../validation/validators/builder/validation-builder';
+import {RequiredFieldValidation} from '../../../../validation/validators/required-field/required-field-validation';
+import {MinLengthValidation} from '../../../../validation/validators/min-length/min-length-validation';
+import {EmailValidation} from '../../../../validation/validators/email/email-validation';
 
 describe('LoginValidationFactory', () => {
   it('Should make ValidationComposite with correct validations', () => {
     const composite = makeLoginValidation();
     expect(composite).toEqual(
       ValidationComposite.build([
-        ...ValidationBuilder.field('email')
-          .required()
-          .email()
-          .build(),
-        ...ValidationBuilder.field('password')
-          .required()
-          .min(5)
-          .build(),
+        new RequiredFieldValidation('email'),
+        new EmailValidation('email'),
+        new RequiredFieldValidation('password'),
+        new MinLengthValidation('password', 5),
       ])
     );
   });
--- src/main/factories/pages/signup/signup-validation-factory.spec.ts
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -1,25 +1,26 @@
 import {ValidationComposite} from '../../../../validation/validators/validation-composite/validation-composite';
-import {ValidationBuilder} from '../../../../validation/validators/builder/validation-builder';
 import {makeSignUpValidation} from './signup-validation-factory';
+import {RequiredFieldValidation} from '../../../../validation/validators/required-field/required-field-validation';
+import {MinLengthValidation} from '../../../../validation/validators/min-length/min-length-validation';
+import {EmailValidation} from '../../../../validation/validators/email/email-validation';
+import {CompareFieldValidation} from '../../../../validation/validators/compare-fields/compare-fields-validation';
 
 describe('SignUpValidationFactory', () => {
   it('Should make ValidationComposite with correct validations', () => {
     const composite = makeSignUpValidation();
     expect(composite).toEqual(
       ValidationComposite.build([
-        ...ValidationBuilder.field('name').required().min(3).build(),
-        ...ValidationBuilder.field('email')
-          .required()
-          .email()
-          .build(),
-        ...ValidationBuilder.field('password')
-          .required()
-          .min(5)
-          .build(),
-        ...ValidationBuilder.field('passwordConfirmation')
-          .required()
-          .sameAs('password')
-          .build(),
+        new RequiredFieldValidation('name'),
+        new MinLengthValidation('name', 3),
+        new RequiredFieldValidation('email'),
+        new EmailValidation('email'),
+        new RequiredFieldValidation('password'),
+        new MinLengthValidation('password', 5),
+        new RequiredFieldValidation('passwordConfirmation'),
+        new CompareFieldValidation(
+          'passwordConfirmation',
+          'password'
+        ),
       ])
     );
   });
