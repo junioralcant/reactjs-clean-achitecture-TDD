319a9cde1da827d34ab5a273ef4a32fab0f90b54
junioralcant <junioralcant111@gmail.com>
2023-04-09 10:39:00 -0300

    fix: move calendar tests back to SurveyList

--- src/presentation/pages/survey-list/components/item/item.spec.tsx
+++ src/presentation/pages/survey-list/components/item/item.spec.tsx
@@ -11,6 +11,7 @@ describe('SurveyItem Component', () => {
   it('Should render with correct values', () => {
     const survey = Object.assign(mockSurveyModel(), {
       didAnswer: true,
+      date: new Date('2022-12-30T00:00:00'),
     });
     makeSut(survey);
 
@@ -21,11 +22,16 @@ describe('SurveyItem Component', () => {
     expect(screen.getByTestId('question')).toHaveTextContent(
       survey.question
     );
+
+    expect(screen.getByTestId('day')).toHaveTextContent('30');
+    expect(screen.getByTestId('month')).toHaveTextContent('dez');
+    expect(screen.getByTestId('year')).toHaveTextContent('2022');
   });
 
   it('Should render with correct values', () => {
     const survey = Object.assign(mockSurveyModel(), {
       didAnswer: false,
+      date: new Date('2021-05-12T00:00:00'),
     });
     makeSut(survey);
 
@@ -36,5 +42,9 @@ describe('SurveyItem Component', () => {
     expect(screen.getByTestId('question')).toHaveTextContent(
       survey.question
     );
+
+    expect(screen.getByTestId('day')).toHaveTextContent('12');
+    expect(screen.getByTestId('month')).toHaveTextContent('mai');
+    expect(screen.getByTestId('year')).toHaveTextContent('2021');
   });
 });
