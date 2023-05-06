4d8a5f608ab1d6f27a036ec08a06e37301ca6d66
junioralcant <junioralcant111@gmail.com>
2023-05-06 16:13:41 -0300

    feat: ensure SurveyList redirect to SurveyResult

--- src/presentation/pages/survey-list/components/item/item.tsx
+++ src/presentation/pages/survey-list/components/item/item.tsx
@@ -1,3 +1,4 @@
+import {useNavigate} from 'react-router-dom';
 import {ILoadSurveyList} from '../../../../../domain/useCases';
 import {Calendar} from '../../../../components/calendar/calendar';
 import {Icon, IconName} from '../../../../components/icon/icon';
@@ -9,9 +10,15 @@ type Props = {
 };
 
 export function SurveyItem({survey}: Props) {
+  const navigate = useNavigate();
+
   const iconName = survey.didAnswer
     ? IconName.thumbUp
     : IconName.thumbDown;
+
+  function navigateSurveyResult() {
+    navigate(`/surveys/${survey.id}`);
+  }
 
   return (
     <li className="surveyItemWrap">
@@ -20,7 +27,9 @@ export function SurveyItem({survey}: Props) {
         <Calendar date={survey.date} className="calendarWrap" />
         <p data-testid="question">{survey.question}</p>
       </div>
-      <footer>Ver resultado</footer>
+      <footer data-testid="link" onClick={navigateSurveyResult}>
+        Ver resultado
+      </footer>
     </li>
   );
 }
