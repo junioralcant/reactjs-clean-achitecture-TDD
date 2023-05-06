f0a7de57adb28be89801fc3eac43d616185ee287
junioralcant <junioralcant111@gmail.com>
2023-05-06 16:13:54 -0300

    test: ensure SurveyList redirect to SurveyResult

--- src/presentation/pages/survey-list/components/item/item.spec.tsx
+++ src/presentation/pages/survey-list/components/item/item.spec.tsx
@@ -1,4 +1,4 @@
-import {render, screen} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import {mockSurveyModel} from '../../../../../domain/test';
 import {IconName} from '../../../../components/icon/icon';
 import {SurveyItem} from './item';
@@ -6,6 +6,12 @@ import {SurveyItem} from './item';
 function makeSut(survey = mockSurveyModel()) {
   render(<SurveyItem survey={survey} />);
 }
+
+const mockedUsedNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...(jest.requireActual('react-router-dom') as any),
+  useNavigate: () => mockedUsedNavigate,
+}));
 
 describe('SurveyItem Component', () => {
   it('Should render with correct values', () => {
@@ -46,5 +52,16 @@ describe('SurveyItem Component', () => {
     expect(screen.getByTestId('day')).toHaveTextContent('12');
     expect(screen.getByTestId('month')).toHaveTextContent('mai');
     expect(screen.getByTestId('year')).toHaveTextContent('2021');
+  });
+
+  it('Should go to SurveyResult', () => {
+    const survey = mockSurveyModel();
+    makeSut(survey);
+
+    fireEvent.click(screen.getByTestId('link'));
+
+    expect(mockedUsedNavigate).toHaveBeenCalledWith(
+      `/surveys/${survey.id}`
+    );
   });
 });
