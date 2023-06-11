71b00825c4580b5a8bea5da1f851f8711bd432eb
junioralcant <junioralcant111@gmail.com>
2023-06-11 09:04:45 -0300

    test: ensure RemoteSaveSurveyResult returns AccessDeniedError on 500

--- src/data/useCases/save-survey-result/remote-save-survey-result.usecase.spec.ts
+++ src/data/useCases/save-survey-result/remote-save-survey-result.usecase.spec.ts
@@ -56,4 +56,15 @@ describe('RemoteSaveSurveyResult', () => {
     const promise = sut.save(mockSaveResultParams());
     await expect(promise).rejects.toThrow(new UnexpectedError());
   });
+
+  it('Should throw UnexpectError if HttpClient return 500', async () => {
+    const {sut, httpClientSpy} = makeSut();
+
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.serverError,
+    };
+
+    const promise = sut.save(mockSaveResultParams());
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
 });
