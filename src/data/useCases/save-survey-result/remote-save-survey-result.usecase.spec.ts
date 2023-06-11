b83cd656e1c5f27913ba4b97ae2b24263fbb88b9
junioralcant <junioralcant111@gmail.com>
2023-06-11 08:54:40 -0300

    test: ensure RemoteSaveSurveyResult calls HttpClient with correct body

--- src/data/useCases/save-survey-result/remote-save-survey-result.usecase.spec.ts
+++ src/data/useCases/save-survey-result/remote-save-survey-result.usecase.spec.ts
@@ -1,6 +1,7 @@
 import {faker} from '@faker-js/faker';
 import {HttpClientSpy} from '../../test';
 import {RemoteSaveSurveyResult} from './remote-save-survey-result.usecase';
+import {mockSaveResultParams} from '../../../domain/test';
 
 type SutTypes = {
   sut: RemoteSaveSurveyResult;
@@ -22,8 +23,10 @@ describe('RemoteSaveSurveyResult', () => {
   it('Should call HttpClient with correct URL and Method', async () => {
     const url = faker.internet.url();
     const {sut, httpClientSpy} = makeSut(url);
-    await sut.save({answers: faker.random.words()});
+    const saveSurveyResultParams = mockSaveResultParams();
+    await sut.save(saveSurveyResultParams);
     expect(httpClientSpy.url).toBe(url);
     expect(httpClientSpy.method).toBe('put');
+    expect(httpClientSpy.body).toBe(saveSurveyResultParams);
   });
 });
